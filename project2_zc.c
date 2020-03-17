224b434bd49eb6eafcc7e88e3f9b0f50d0317692
tzchuen <54998225+tzchuen@users.noreply.github.com>
2020-03-16 17:02:57 -0700

    added old proj 2 file

---
+++ project2_zc.c
@@ -0,0 +1,347 @@
+// Standalone program
+
+// Preprocessor directives
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <EFM8LB1.h>
+
+// ~C51~  
+
+#define SYSCLK 72000000L
+#define BAUDRATE 115200L
+
+// LCD pins
+#define LCD_RS P2_6
+// #define LCD_RW Px_x // Not used in this code.  Connect to GND
+#define LCD_E  P2_5
+#define LCD_D4 P2_4
+#define LCD_D5 P2_3
+#define LCD_D6 P2_2
+#define LCD_D7 P2_1
+
+#define TRUE 1
+#define FALSE 0
+
+#define CHARS_PER_LINE 16
+#define ANSI_CLEAR_SCREEN "\x1b[2J"
+#define ANSI_CURSOR_END_LINE "\x1b[0K"
+
+#define TIMER_0_RESULT TH0*256.0+TL0
+
+#define DETECTOR_IN  QFP32_MUX_P1_7
+#define VCC 5
+// #define NORMAL_FREQ   0.0
+// #define ERROR_TOLERANCE  0 // Hz
+
+volatile unsigned char timer0_count_ms = 0;
+volatile unsigned char period = 0;
+
+
+
+//  C51 
+char _c51_external_startup (void)
+{
+	// Disable Watchdog with key sequence
+	SFRPAGE = 0x00;
+	WDTCN = 0xDE; //First key
+	WDTCN = 0xAD; //Second key
+  
+	VDM0CN=0x80;       // enable VDD monitor
+	RSTSRC=0x02|0x04;  // Enable reset on missing clock detector and VDD
+
+	#if (SYSCLK == 48000000L)	
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
+		SFRPAGE = 0x00;
+	#elif (SYSCLK == 72000000L)
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
+		SFRPAGE = 0x00;
+	#endif
+	
+	#if (SYSCLK == 12250000L)
+		CLKSEL = 0x10;
+		CLKSEL = 0x10;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 24500000L)
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 48000000L)	
+		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x07;
+		CLKSEL = 0x07;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 72000000L)
+		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x03;
+		CLKSEL = 0x03;
+		while ((CLKSEL & 0x80) == 0);
+	#else
+		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+	#endif
+	
+	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
+	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
+	XBR1     = 0X00;
+	XBR2     = 0x40; // Enable crossbar and weak pull-ups
+
+	// Configure Uart 0
+	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
+		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+	#endif
+	SCON0 = 0x10;	// Timer2 uses SYSCLK; Timer1, Timer0 use SYSCLK/12
+	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
+	TL1 = TH1;      // Init Timer1
+	
+	/* Timer1: not gated bit[7], timer mode [6], 8-bit auto reload [5:4]
+	 * Timer0: gated [3], timer mode [2], 16-bit [1:0] 
+	 */
+	TMOD &= ~0xff;  // clear all bits
+	TMOD |=  0x29;   
+
+	/* 1ms delay for Timer0:
+	 * [TH0:TL0] = 2^16 - ((SYSCLK/12)/(1/1us)) = 65530
+	 */
+	TH0 = 0xff;
+	TL0 = 0xfa;
+
+	TR1 = 1; // START Timer1
+	TR0 = 1; // START Timer0
+	TI = 1;  // Indicate TX0 ready
+
+	// External interrupts INT0 active low [3], P0_7 [2:0] selected
+	IT01CF &= ~0x07; // clear bits [3:0] 
+	IT01CF |=  0x07; // set bits [3:0] 
+	
+	// Interrupts
+	TCON &= ~0x03;	// clear bits [2:0]
+	TCON |=  0x01;  // set bit 0 - INT0 edge triggered
+
+	// ^ Timer0 triggers on negedge
+
+	ET0 = 1;	   // Enable Timer0 interrupt
+	EX0 = 1;	   // Enable external interrupt 0
+	EA = 1;
+
+	return 0;
+}
+
+void pulse_width_isr (void) interrupt INTERRUPT_TIMER0
+{
+	TF0 = 0; // clear Timer0 flag
+	timer0_count_ms = 0;
+
+	timer0_count_ms++;
+}
+
+void InitADC (void)
+{
+	SFRPAGE = 0x00;
+	ADC0CN1 = 0b_10_000_000; //14-bit,  Right justified no shifting applied, perform and Accumulate 1 conversion.
+	ADC0CF0 = 0b_11111_0_00; // SYSCLK/32
+	ADC0CF1 = 0b_0_0_011110; // Same as default for now
+	ADC0CN0 = 0b_0_0_0_0_0_00_0; // Same as default for now
+	ADC0CF2 = 0b_0_01_11111 ; // GND pin, Vref=VDD
+	ADC0CN2 = 0b_0_000_0000;  // Same as default for now. ADC0 conversion initiated on write of 1 to ADBUSY.
+	ADEN=1; // Enable ADC
+}
+
+// Uses Timer3 to delay <us> micro-seconds. 
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
+	CKCON0|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN0 & 0x80));  // Wait for overflow
+		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+	}
+	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	unsigned char k;
+	for(j=0; j<ms; j++)
+		for (k=0; k<4; k++) Timer3us(250);
+}
+
+#define VDD 3.3035 // The measured value of VDD in volts
+
+void InitPinADC (unsigned char portno, unsigned char pinno)
+{
+	unsigned char mask;
+	
+	mask=1<<pinno;
+
+	SFRPAGE = 0x20;
+	switch (portno)
+	{
+		case 0:
+			P0MDIN &= (~mask); // Set pin as analog input
+			P0SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		case 1:
+			P1MDIN &= (~mask); // Set pin as analog input
+			P1SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		case 2:
+			P2MDIN &= (~mask); // Set pin as analog input
+			P2SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+		default:
+		break;
+	}
+	SFRPAGE = 0x00;
+}
+
+unsigned int ADC_at_Pin(unsigned char pin)
+{
+	ADC0MX = pin;   // Select input from pin
+	ADBUSY=1;       // Dummy conversion first to select new pin
+	while (ADBUSY); // Wait for dummy conversion to finish
+	ADBUSY = 1;     // Convert voltage at the pin
+	while (ADBUSY); // Wait for conversion to complete
+	return (ADC0);
+}
+
+float Volts_at_Pin(unsigned char pin)
+{
+	 return ((ADC_at_Pin(pin)*VDD)/0b_0011_1111_1111_1111);
+}
+
+void LCD_pulse (void)
+{
+	LCD_E=1;
+	Timer3us(40);
+	LCD_E=0;
+}
+
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x; //Send high nible
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_pulse();
+	Timer3us(40);
+	ACC=x; //Send low nible
+	LCD_D7=ACC_3;
+	LCD_D6=ACC_2;
+	LCD_D5=ACC_1;
+	LCD_D4=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_4BIT (void)
+{
+	LCD_E=0; // Resting state of LCD's enable is zero
+	// LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x32); // Change to 4-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x28);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	int j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
+
+int getsn (char * buff, int len)
+{
+	int j;
+	char c;
+	
+	for(j=0; j<(len-1); j++)
+	{
+		c=getchar();
+		if ( (c=='\n') || (c=='\r') )
+		{
+			buff[j]=0;
+			return j;
+		}
+		else
+		{
+			buff[j]=c;
+		}
+	}
+	buff[j]=0;
+	return len;
+}
+
+// void Timer0_init (void)
+// {
+//     // initialize timer 0
+// 	TMOD&=0b_1111_0000; // Set the bits of Timer/Counter 0 to zero
+// 	TMOD|=0b_0000_0001; // Timer/Counter 0 used as a 16-bit timer
+// 	TR0=0; // Stop Timer/Counter 0
+// }
+
+
+void main (void) 
+{
+    bool coin_found = false;
+    xdata double detected_freq = 0.0;
+    xdata double detected_period = 0.0;
+
+    LCD_4BIT();
+    Timer0_init();
+
+    InitPinADC(1, 7);
+    InitADC();
+
+    printf(ANSI_CLEAR_SCREEN);
+    printf("Metal detector circuit\n");
+    printf("Compiled: %s, %s\n\n", __DATE__, __TIME__);
+
+    while (1)
+    {
+    }
+}
+
+
