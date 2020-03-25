933444120267b1f258d16bd29504697150b2c6c8
tzchuen <54998225+tzchuen@users.noreply.github.com>
2020-03-24 20:48:21 -0700

    compiles, not much else

--- project2.c/project2.c/main.c
+++ project2.c/project2.c/main.c
@@ -1,3 +1,7 @@
+#define F_CPU 16000000UL
+#define BAUD 19200
+#define MYUBRR F_CPU/16/BAUD-1
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
@@ -10,30 +14,30 @@
 #define BAUD 19200
 #define MYUBRR F_CPU/16/BAUD-1
 
-// Signal strength LEDs
-#define YELLOW_LED_1	PORTD4
-#define YELLOW_LED_2	PORTD3
-#define RED_LED_METER   PORTD2
-
-// Push buttons
-#define BUTTON_LEFT_MUTE			PINC3
-#define BUTTON_MID_LED				PINC4
-#define BUTTON_RIGHT_LED_STRENGTH	PINC5
-
-// LED/Speaker 
-#define DETECTOR_LED	PORTB2
-#define SPEAKER			PORTB1
-
-
-// Detector input
-#define METAL_DETECT_IN	PIND4
+//// Signal strength LEDs
+//#define YELLOW_LED_1	PORTD4
+//#define YELLOW_LED_2	PORTD3
+//#define RED_LED_METER   PORTD2
+//
+//// Push buttons
+//#define BUTTON_LEFT_MUTE			PINC3
+//#define BUTTON_MID_LED				PINC4
+//#define BUTTON_RIGHT_LED_STRENGTH	PINC5
+//
+//// LED/Speaker 
+//#define DETECTOR_LED	PORTB2
+//#define SPEAKER			PORTB1
+//
+//
+//// Detector input
+//#define METAL_DETECT_IN	PIND4
 
 // PuTTY
 #define ANSI_CLEAR_SCREEN "\x1b[2J"
 #define ANSI_CURSOR_END_LINE "\x1b[0K"
 
-#define LOGIC_HIGH	1
-#define LOGIC_LOW	0
+//#define LOGIC_HIGH	1
+//#define LOGIC_LOW	0
 
 volatile uint16_t period;
 
@@ -52,16 +56,6 @@ void waitms(int ms)
 		wait_1ms();
 }
 
-/* DDxn bit in the DDRx Register selects the direction of this pin. If DDxn is written logic one,
- * Pxn is configured as an output pin. If DDxn is written logic zero, Pxn is configured as an input
- * pin.
- * 
- * If PORTxn is written logic one when the pin is configured as an input pin, the pull-up resistor is
- * activated.
- * 
- * If PORTxn is written logic one when the pin is configured as an output pin, the port pin is driven
- * high (one).
- */
 unsigned int ReadChannel(unsigned char mux)
 {
 	ADCSRA = (1<<ADEN) | (1<<ADPS1) | (1<<ADPS0); // frequency prescaler
@@ -78,13 +72,73 @@ unsigned int ReadChannel(unsigned char mux)
 // Input Capture Unit Interrupt at ICP1 (Pin 14)
 ISR (TIMER1_CAPT_vect) 
 {
-	while (ICF1 == 0);			// Wait for capture
-	period = ICR1;				// Get timestamp
-	ICF1 = 0;					// Clear flag
-	while (ICF1 == 0);			// Wait for second capture
-	period = ICR1 - period;		// Period = time between 2 timestamps
-	ICF1 = 0;					// Clear flag
+	while ((TIFR1&1<<ICF1) == 0);	// Wait for capture
+	period = ICR1;					// Get timestamp
+	TIFR1 = 1<<ICF1;				// Clear flag
+	while ((TIFR1&1<<ICF1) == 0);	// Wait for second capture
+	period = ICR1 - period;			// Period = time between 2 timestamps
+	TIFR1 = 1<<ICF1;				// Clear flag
 }
+
+/* DDxn bit in the DDRx Register selects the direction of this pin. If DDxn is written logic one,
+ * Pxn is configured as an output pin. If DDxn is written logic zero, Pxn is configured as an input
+ * pin.
+ * 
+ * If PORTxn is written logic one when the pin is configured as an input pin, the pull-up resistor is
+ * activated.
+ * 
+ * If PORTxn is written logic one when the pin is configured as an output pin, the port pin is driven
+ * high (one).
+ */
+
+/***********************************************************
+ * PORTX registers (X=B,C,D) to assign output states	   *
+ * ______________________________________________          *
+ * Bit	| 7  | 6  | 5  | 4  | 3  | 2  | 1  | 0  |          *
+ * Pin  | X7 | X6 | X5 | X4 | X3 | X2 | X1 | X0 |          *
+ * ----------------------------------------------          *
+ *													       *
+ * Note: Can't directly access registers; need to |= or &= *
+ * to set bits											   *
+ ***********************************************************/
+
+/******************************************************
+ * TCCR1B for Input Capture                           *
+ * ___________________________________________        *
+ * Bit	|  7  |  6  | 5 | 4 | 3  | 2 | 1 | 0 |        *
+ * Pin  |ICNC1|ICES1| - |Waveform| CLK select|        *
+ * -------------------------------------------        *
+ *													  *
+ * ICNC1 = Noise canceler                             *
+ * ICES1 = Edge select (pos/negedge)                  *
+ * 								                      *
+ ******************************************************/
+
+/*************************
+ *      CLK select	     *
+ * _____________________ *
+ * | 0x00 | Stop       | *
+ * | 0x01 | clk/1      | *
+ * | 0x02 | clk/8      | *
+ * | 0x03 | clk/64     | *
+ * | 0x04 | clk/256    | *
+ * | 0x05 | clk/1024   | *
+ * | 0x06 | T1 negedge | *
+ * | 0x07 | T1 posedge | *
+ * --------------------- *
+ *************************/
+
+/******************************************************
+ * TIFR1 for Timer1 Interrupt Flags                   *
+ * ___________________________________________        *
+ * Bit	| 7 | 6 |  5   | 4 | 3 | 2 | 1 |  0  |        *
+ * Pin  | - | - | ICF1 |   -   |Compare|TOV1 |        *
+ * -------------------------------------------        *
+ *													  *
+ * ICF1 = Input capture flag                          *
+ * TOV1 = Timer1 ovf flag                             *
+ * 								                      *
+ ******************************************************/
 
 int main(void)
 {	
@@ -92,9 +146,9 @@ int main(void)
 	
 	double frequency;
 	
-	waitms(500);
+	waitms(500);	// Give PuTTY a chance to start
 	
-	//usart_init();	
+	usart_init();	
 	
 	printf(ANSI_CLEAR_SCREEN);
 	printf("Metal Detector System v1.0\n"
@@ -117,37 +171,35 @@ int main(void)
 	TCCR1B |= 0x41; // no noise canceler; capture at rising edge; no prescaling
 	TIFR1 &= ~(0x20); // clear input capture flag
 	
-	
-	
-	
 	// Pin/Port configuration
 	DDRB  |= 0x07; // Set B0, 1, 2 (outputs, Pin 14-16)
 	DDRC  |= 0x00; // All C pins are inputs
 	PORTC |= 0x38; // Set pull-ups for push buttons
 	DDRD  |= 0x1c; // Set D2, D3, D4 (outputs, Pin 4-6)
 	
-	// Output sanity checks
-	YELLOW_LED_1 = LOGIC_HIGH;
-	YELLOW_LED_2 = LOGIC_HIGH;
-	RED_LED_METER = LOGIC_HIGH
-	DETECTOR_LED = LOGIC_HIGH;
-	SPEAKER = LOGIC_HIGH;
-	
-	waitms(100);
-	
-	YELLOW_LED_1 = LOGIC_LOW;
-	YELLOW_LED_2 = LOGIC_LOW;
-	RED_LED_METER = LOGIC_LOW;
-	DETECTOR_LED = LOGIC_LOW;
-	SPEAKER = LOGIC_LOW;
+	/* Outputs:
+	 * YellowL D4
+	 * YellowM D3
+	 * Red	   D2
+	 * Green   B2
+	 * Speaker B1
+	 */
+
+	// Output LED/speaker sanity check
+	PORTD |= 0x1c;	// turn on small LEDs
+	PORTB |= 0x06;	// turn on speaker and green LED
+	waitms(200);
+	PORTD &= ~(0x1c);	// turn off small LEDs
+	PORTB &= ~(0x06);	// turn off speaker and green LED
 	
 	printf("Initialization complete!\n");
 	printf(ANSI_CURSOR_END_LINE);
 	sei();	// enable interrupts
-	
-    while (1) 
+    
+	while (1) 
     {
 		frequency = (double) F_CPU / period;
+		printf("\r F= %fHz", frequency);
     }
 }	
 
